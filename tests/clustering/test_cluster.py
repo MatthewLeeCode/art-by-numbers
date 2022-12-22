dcf92dd51c6c24010d1ac0da44d0703f484c77f6
Matthew Lee <matthewleemattner@gmail.com>
2022-12-22 16:38:26 +1000

    Updated clustering tests for further coverage of functionality.

--- tests/clustering/test_cluster.py
+++ tests/clustering/test_cluster.py
@@ -3,7 +3,6 @@ Tests the functions of the clustering module.
 """
 import clustering
 import numpy as np
-from PIL import Image
 
 
 def create_test_image():
@@ -17,65 +16,113 @@ def create_test_image():
     Returns:
         np.ndarray: The test image.
     """
+    # Dictionary of the coordinates and colors in those coordinates
+    color_locations = {
+        (0, 0): (255, 0, 0),
+        (0, 2): (0, 0, 255),
+        (2, 2): (0, 255, 0),
+        (2, 0): (255, 255, 255)
+    }
+    
     # Create a test image
-    image = np.zeros((1, 3, 3), dtype=np.uint8)
+    image = np.zeros((3, 3, 3), dtype=np.uint8)
     
-    # Set the top left pixel to red
-    image[0, 0, :] = [255, 0, 0]
+    # Assign those colors to the image
+    for location, color in color_locations.items():
+        image[location] = color
     
-    # Set the last pixel to blue
-    image[0, 2, :] = [0, 0, 255]
-    
-    return image
-
+    return image, color_locations
 
 def test_kmeans_cluster():
     """ Tests the kmeans_cluster function. """
     # Create a test image
-    image = create_test_image()
+    image, _ = create_test_image()
     
     # Perform the K-means clustering
-    cluster_dict, clustered_image = clustering.kmeans_cluster(image, 3)
+    cluster_dict, clustered_image = clustering.kmeans_cluster(image, 5)
     
     # Check that the cluster dictionary contains the correct number of clusters
-    assert len(cluster_dict) == 3
+    assert len(cluster_dict) == 5
     
     # Check that the cluster dictionary contains the correct colors
     # The order of the colors is not guaranteed
     assert (0, 0, 0) in cluster_dict.values()
     assert (0, 0, 255) in cluster_dict.values()
     assert (255, 0, 0) in cluster_dict.values()
+    assert (0, 255, 0) in cluster_dict.values()
+    assert (255, 255, 255) in cluster_dict.values()
 
     # Check that no other colors are in the cluster dictionary
-    assert len(cluster_dict.values()) == 3
+    assert len(cluster_dict.values()) == 5
     
     # Check that clustered image matches the original image
     assert np.array_equal(image, np.array(clustered_image))
     
-    
 def test_create_mask():
     """ Tests the create_mask function. """
     # Create a test image
-    image = create_test_image()
+    image, color_locations = create_test_image()
     
-    # Test the create_mask function for the red color
-    expected_image = np.zeros((1, 3), dtype=np.uint8)
+    # Loop through the color locations and create a mask for each color
+    for location, color in color_locations.items():
+        mask = clustering.create_mask(image, color)
+        
+        # Check that the mask matches the expected image
+        expected_mask = np.zeros((3, 3), dtype=np.uint8)
+        
+        # A value of '1' should be at the position of the color
+        expected_mask[location] = 1
+        
+        # Assert that the masks are equal
+        assert np.array_equal(expected_mask, np.array(mask))
+
+def test_create_mask_with_many_color():
+    """ Tests that create_mask function can handle many of the same color """
+    # Create a test image
+    image, _ = create_test_image()
     
-    # Expects the first pixel to be '1' and the rest to be '0'
-    expected_image[0, 0] = 1
+    # Add another red point to the image
+    image[1, 1] = (255, 0, 0)
     
+    # Create the mask for the red color
     mask = clustering.create_mask(image, (255, 0, 0))
-
-    # Check that the mask matches the expected image
-    assert np.array_equal(expected_image, np.array(mask))
-    
-    # Test the create_mask function for the blue color
-    expected_image = np.zeros((1, 3), dtype=np.uint8)
-
-    # Expects the last pixel to be '1' and the rest to be '0'
-    expected_image[0, 2] = 1
-
-    mask = clustering.create_mask(image, (0, 0, 255))
     
     # Check that the mask matches the expected image
-    assert np.array_equal(expected_image, np.array(mask))
+    expected_mask = np.zeros((3, 3), dtype=np.uint8)
+    expected_mask[0, 0] = 1
+    expected_mask[1, 1] = 1
+    assert np.array_equal(expected_mask, np.array(mask))
+
+def test_clustering_integration():
+    """
+    Integration test for the clustering module.
+    
+    Tests:
+        - kmeans_cluster
+        - create_mask
+    """
+    # Create a test image
+    image, _ = create_test_image()
+    
+    # Add another red point to the image
+    image[1, 1] = (255, 0, 0)
+    
+    # Perform the K-means clustering
+    _, clustered_image = clustering.kmeans_cluster(image, 5)
+    
+    # Create a mask for the red color
+    mask = clustering.create_mask(clustered_image, (255, 0, 0))
+    
+    # Check that the mask matches the expected image
+    expected_mask = np.zeros((3, 3), dtype=np.uint8)
+    expected_mask[0, 0] = 1
+    expected_mask[1, 1] = 1
+    assert np.array_equal(expected_mask, np.array(mask))
+    
+    # Create a mask for the blue color
+    mask = clustering.create_mask(clustered_image, (0, 0, 255))
+    
+    # Check that the mask matches the expected image
+    expected_mask = np.zeros((3, 3), dtype=np.uint8)
+    expected_mask[0, 2] = 1
+    assert np.array_equal(expected_mask, np.array(mask))
