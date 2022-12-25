21cb77345c6bcf82caeb9db3821e89f0bf0d5bb1
Matthew Lee <matthewleemattner@gmail.com>
2022-12-25 11:26:07 +1000

    Updated visual center test

--- tests/labelling/test_label_placement.py
+++ tests/labelling/test_label_placement.py
@@ -2,8 +2,8 @@ import labelling
 import numpy as np
 
 
-def test_find_representative_point() -> None:
-    """ Test the find_representative_point function. 
+def test_find_visual_center() -> None:
+    """ Test the find_visual_center function. 
     
                           Centroid
                          │
@@ -20,12 +20,12 @@ def test_find_representative_point() -> None:
             │
         Representative Point
         
-    Tests that the representative point is placed as expected
+    Tests that the visual center point is placed as expected
     """
-    shell = np.array([[0, 0], [0, 1], [1, 1], [1, 0]])
-    holes = [np.array([[0.5, 0.2], [0.5, 0.8], [0.8, 0.8], [0.8, 0.2]])]
+    shell = np.array([[[0, 0]], [[0, 1]], [[1, 1]], [[1, 0]]])
+    holes = [np.array([[[0.5, 0.2]], [[0.5, 0.8]], [[0.8, 0.8]], [[0.8, 0.2]]])]
     
     
     expected_point = np.array([0.25, 0.5])
-    point = labelling.find_representative_point(shell, holes)
+    point = labelling.find_visual_center(shell, holes)
     assert np.allclose(point, expected_point)
